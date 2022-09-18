a645cb290f381f84e451539e9f92c94b40649436
Jhonatan Nascimento <jhonatanfnm@gmail.com>
2022-09-18 11:48:47 -0300

    Criando a estruturara basica da aplicação na mão

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import "./styles/global.scss";
+
 export function App() {
-  return <h1>Hello World|</h1>;
+  // throw new Error("Eita giovana o forninho caiu");
+
+  return <h1>Hello ReactJS</h1>;
 }
